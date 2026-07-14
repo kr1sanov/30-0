40d76539704bde2d02d2b152bc8d0418d2e0a216
kr1sanov <kr1sanov@users.noreply.github.com>
2026-07-14 06:22:09 +0000

    feat(analytics): add Yandex.Metrika counter (ID: 110726199)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster as SonnerToaster } from "sonner";
@@ -41,6 +42,21 @@ export default function RootLayout({
         {/* Telegram WebApp SDK */}
         <script src="https://telegram.org/js/telegram-web-app.js" async />
       </head>
+      {/* Yandex.Metrika counter */}
+      <Script id="yandex-metrika" strategy="afterInteractive">
+        {`(function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110726199', 'ym');
+
+        ym(110726199, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`}
+      </Script>
+      <noscript>
+        <div><img src="https://mc.yandex.ru/watch/110726199" style={{position:'absolute', left:'-9999px'}} alt="" /></div>
+      </noscript>
+      {/* /Yandex.Metrika counter */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0A0A0A] text-[#FFFFFF]`}
         style={{ paddingTop: 'env(safe-area-inset-top)', paddingBottom: 'env(safe-area-inset-bottom)', paddingLeft: 'env(safe-area-inset-left)', paddingRight: 'env(safe-area-inset-right)' }}
